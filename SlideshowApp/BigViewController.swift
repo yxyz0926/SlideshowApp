a8be9ddb2973d7d08c5ebe41c94339c44baf8516
yuu <yxyz0926@gmail.commkdir ~/.sshtouch ~/.ssh/configchmod 700 ~/.sshchmod 600 ~/.ssh/*xattr -cr ~/.ssh>
2020-12-21 22:16:55 +0900

    コミットした。

---
+++ SlideshowApp/BigViewController.swift
@@ -0,0 +1,37 @@
+//
+//  BigViewController.swift
+//  SlideshowApp
+//
+//  Created by Yu iwawaki on 2020/12/21.
+//  Copyright © 2020 Yu iwawaki. All rights reserved.
+//
+
+import UIKit
+
+class BigViewController: UIViewController {
+    @IBOutlet weak var bigImageView: UIImageView!
+    
+    
+    var x = ""
+
+    override func viewDidLoad() {
+        
+        bigImageView.image = UIImage(named: x)
+        
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,12 +9,123 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var backImageButton: UIButton!
+    @IBOutlet weak var slideButton: UIButton!
+    @IBOutlet weak var nextImageButton: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imageName = ["owl_pic.jpg", "dog_pic.jpg", "squirrel_pic.jpg"]
+    var changeImageNo = 0
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imageView.image = UIImage(named: imageName[0])
+        
     }
+    
+    
+    @IBAction func nextImage(_ sender: Any) {
+        
+        changeImageNo += 1
+        
+        if changeImageNo == 3 {
+            changeImageNo = 0
+            
+        }
+        
+        imageView.image = UIImage(named: imageName[changeImageNo])
+        
+    }
+    
+    
+    @IBAction func backImage(_ sender: Any) {
+        
+        changeImageNo -= 1
+        
+        if changeImageNo == -1 {
+            changeImageNo = 2
+        }
+        
+        imageView.image = UIImage(named: imageName[changeImageNo])
+        
+    }
+    
+    //タイマー
+    var timer: Timer!
+    
+    @objc func changeImage(){
+        
+        changeImageNo += 1
+        
+        if changeImageNo == imageName.count {
+            changeImageNo = 0
+        }
+        self.imageView.image = UIImage(named: imageName[changeImageNo])
+        
+    }
+    
+
+    
+    @IBAction func startOrStop(_ sender: Any) {
+        
+        if self.timer == nil {
+            self.timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(changeImage), userInfo: nil, repeats: true)
+            
+            slideButton.setTitle("停止", for: .normal)
+            
+            nextImageButton.isEnabled = false
+            backImageButton.isEnabled = false
+
+            
+        } else {
+            self.timer.invalidate()
+            
+            self.timer = nil
+            
+            slideButton.setTitle("再生", for: .normal)
+            
+            nextImageButton.isEnabled = true
+            backImageButton.isEnabled = true
+        }
+        
+    }
+        
+    @IBAction func tapImage(_ sender: Any) {
+
+        switch changeImageNo {
+        case 0:
+            self.performSegue(withIdentifier: "toTap" , sender: nil)
+        case 1:
+            self.performSegue(withIdentifier: "toTap" , sender: nil)
+
+        default:
+            self.performSegue(withIdentifier: "toTap" , sender: nil)
+        }
+
+        
+        }
+
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        
+        if segue.identifier == "toTap" {
+
+        let bigViewController = segue.destination as! BigViewController
+
+            bigViewController.x = imageName[changeImageNo]
+
+        }
+
+        }
+
+    
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue){
+}
+
+
 
 
 }
-
