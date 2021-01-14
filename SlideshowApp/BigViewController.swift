fd2e84164aade1e44dffea7e4ad52e75d2a8dfe5
yuu <yxyz0926@gmail.commkdir ~/.sshtouch ~/.ssh/configchmod 700 ~/.sshchmod 600 ~/.ssh/*xattr -cr ~/.ssh>
2021-01-14 20:08:02 +0900

    コメントなど余計なコードを消した。

--- SlideshowApp/BigViewController.swift
+++ SlideshowApp/BigViewController.swift
@@ -11,28 +11,14 @@ import UIKit
 class BigViewController: UIViewController {
     @IBOutlet weak var bigImageView: UIImageView!
     
-    
     var x = ""
 
     override func viewDidLoad() {
         
         bigImageView.image = UIImage(named: x)
         
-        
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
